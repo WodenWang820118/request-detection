f1b70029d72df32878324ece737a21589d685f1b
Guan-Xin <alex0972112871@gmail.com>
2022-12-23 18:37:55 +0800

    func: add font color; clean up

--- src/config.ts
+++ src/config.ts
@@ -1,97 +1,132 @@
 export const URL = 'https://www.msi.com/';
-export const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
+export const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
 export const HEADERLESS = true;
 export const NUM_SLEEP = 1000;
 
 export const shiseidoUrls = [
   {
     url: 'https://international.shiseido.co.jp/',
-    gtmId: 'G-6DMK62Q163'
+    gtmId: 'G-6DMK62Q163',
   },
   {
     url: 'https://www.shiseido.com.hk/',
-    gtmId: 'G-NNDLS9GWJ0'
+    gtmId: 'G-NNDLS9GWJ0',
   },
   {
     url: 'https://www.shiseido.id/',
-    gtmId: 'G-LNPCTBPHRP'
+    gtmId: 'G-LNPCTBPHRP',
   },
   {
     url: 'https://brand.shiseido.co.jp/',
-    gtmId: 'G-D74940R4HS'
+    gtmId: 'G-D74940R4HS',
   },
   {
     url: 'https://www.shiseido.co.kr/',
-    gtmId: 'G-7781YN8C5Q'
+    gtmId: 'G-7781YN8C5Q',
   },
   {
     url: 'https://www.shiseido.com.my/',
-    gtmId: 'G-PRQD0Z00JW'
+    gtmId: 'G-PRQD0Z00JW',
   },
   {
     url: 'https://www.shiseido.com.sg/',
-    gtmId: 'G-P6GMYNTS8Q'
+    gtmId: 'G-P6GMYNTS8Q',
   },
   {
     url: 'https://www.shiseido.co.th/',
-    gtmId: 'G-061NNEPRDN'
+    gtmId: 'G-061NNEPRDN',
   },
   {
     url: 'https://www.global-shiseido.com.tw/',
-    gtmId: 'G-GS9KNERPG1'
+    gtmId: 'G-GS9KNERPG1',
   },
   {
     url: 'https://www.shiseido.com.vn/',
-    gtmId: 'G-LV1GGRQGEY'
-  }
+    gtmId: 'G-LV1GGRQGEY',
+  },
 ];
 
 export const cpbUrls = [
   {
     url: 'https://www.cledepeau-beaute.com.hk/',
-    gtmId: 'G-5YMGWVCH2X'
+    gtmId: 'G-5YMGWVCH2X',
   },
   {
     url: 'https://www.cledepeau-beaute.com/id/',
-    gtmId: 'G-5JQTWBDTF5'
+    gtmId: 'G-5JQTWBDTF5',
   },
   {
     url: 'https://www.cledepeau-beaute.com/jp/',
-    gtmId: 'G-2M80P37MB9'
+    gtmId: 'G-2M80P37MB9',
   },
   {
     url: 'https://www.cledepeau-beaute.com/kr/',
-    gtmId: 'G-XXVWS6MSEF'
+    gtmId: 'G-XXVWS6MSEF',
   },
   {
     url: 'https://www.cledepeau-beaute.com/my/',
-    gtmId: 'G-7SS4ZVFSC4'
+    gtmId: 'G-7SS4ZVFSC4',
   },
   {
     url: 'https://www.cledepeau-beaute.com/sg/',
-    gtmId: 'G-JHP8NRFHRZ'
+    gtmId: 'G-JHP8NRFHRZ',
   },
   {
     url: 'https://www.cledepeau-beaute.co.th/',
-    gtmId: 'G-G7GNWPM90C'
+    gtmId: 'G-G7GNWPM90C',
   },
   {
     url: 'https://www.cledepeau-beaute.com.tw/',
-    gtmId: 'G-K3579K07H3'
+    gtmId: 'G-K3579K07H3',
   },
   {
     url: 'https://www.cledepeau-beaute.com/vn/',
-    gtmId: 'G-T3WY7DD03G'
+    gtmId: 'G-T3WY7DD03G',
   },
   {
-    url: 'https://www.cledepeau-beaute.com/int/philippine-store.html ',
-    gtmId: 'G-NZBP1T8WH8'
+    url: 'https://www.cledepeau-beaute.com/int/philippine-store.html',
+    gtmId: 'G-NZBP1T8WH8',
+  },
+  {
+    url: 'https://development.cledepeau-beaute.com.tw/home',
+    gtmId: 'G-2WW1FLC4M9',
+  },
+  {
+    url: 'https://development.cledepeau-beaute.co.th/home',
+    gtmId: 'G-YCX2T8375J',
+  },
+  {
+    url: 'https://development.cledepeau-beaute.com.hk/zh/home',
+    gtmId: 'G-665C05FXE7',
   },
 ];
 
-// TODO: how to log in to the website?
-export const contentsquare = [
-  {
-    url: 'https://www.chanel.com/kr/',
-  }
-];
+// console font colors
+export const Reset = '\x1b[0m';
+export const Bright = '\x1b[1m';
+export const Dim = '\x1b[2m';
+export const Underscore = '\x1b[4m';
+export const Blink = '\x1b[5m';
+export const Reverse = '\x1b[7m';
+export const Hidden = '\x1b[8m';
+
+export const FgBlack = '\x1b[30m';
+export const FgRed = '\x1b[31m';
+export const FgGreen = '\x1b[32m';
+export const FgYellow = '\x1b[33m';
+export const FgBlue = '\x1b[34m';
+export const FgMagenta = '\x1b[35m';
+export const FgCyan = '\x1b[36m';
+export const FgWhite = '\x1b[37m';
+export const FgGray = '\x1b[90m';
+
+export const BgBlack = '\x1b[40m';
+export const BgRed = '\x1b[41m';
+export const BgGreen = '\x1b[42m';
+export const BgYellow = '\x1b[43m';
+export const BgBlue = '\x1b[44m';
+export const BgMagenta = '\x1b[45m';
+export const BgCyan = '\x1b[46m';
+export const BgWhite = '\x1b[47m';
+export const BgGray = '\x1b[100m';
